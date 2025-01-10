983532e00ffc8c2f1f8fc57b6f3e22ba9933ad9a
Dra5me <moustaphadraame1@gmail.com>
2025-01-10 11:03:25 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,17 @@
-# Écrivez votre code ici !
+creer les quatres variables
+nom="Drame"
+age=18
+taille=1.79
+est_etudiant=true
+
+afficher les valeurs des quatres variables
+print(f"nom: {nom}")
+print(f"age: {age}")
+print(f"taille: {taille}")
+print(f"est etudiant: {est_etudiant}")
+
+affficher les types des quatres valeurs variables
+print(f"Type nom: {Type(nom)}")
+print(f"Type age: {Type(age)}")
+print(f"Type taille: {Type(taille)}")
+print(f"Type est etudiant: {Type(est_etudant)}")
